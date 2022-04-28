bc393f50ca27b255d7755eb6fc988fc1f8c65483
JUNWOOJOK <78505823+JUNWOOJOK@users.noreply.github.com>
2022-04-29 00:00:53 +0900

    Update trimmomatic.sh

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -1,12 +1,14 @@
-cat trimmomatic_bash 
+find
 for i in $(ls|grep TCGA|grep -v bash)
 do cd $i
-trimmomatic PE -threads 7  repair.1.fq repair.2.fq forward_paired.fq forward_unpaird.fq reverse_paired.fq reverse_unpaird.fq ILLUMINACLIP::2:30:10:2:True LEADING:3 TRAILING:3 &
-trimmomatic SE -threads 7  repair.s.fq single_end.fq ILLUMINACLIP::2:30:10:2:True LEADING:3 TRAILING:3 &
+trimmomatic PE -threads 7  repair.1.fq repair.2.fq forward_paired.fq forward_unpaird.fq reverse_paired.fq reverse_unpaird.fq ILLUMINACLIP:/home/junwoojo/donefile_energy/adapter:2:30:10:2:True LEADING:3 TRAILING:3 &
+trimmomatic SE -threads 7  repair.s.fq single_end.fq ILLUMINACLIP:/home/junwoojo/donefile_energy/adapter:2:30:10:2:True LEADING:3 TRAILING:3 &
 wait
 mkdir trimmomatic
 mv forward* trimmomatic/
 mv reverse* trimmomatic/
 mv single* trimmomatic/
-cd ..
+cd trimmomatic/
+cat forward_unpaird.fq reverse_unpaird.fq single_end.fq > singleton.fq
+cd ../../
 done
