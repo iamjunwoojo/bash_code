c77f0937fc54ef7983fec42dad638590f232b3e3
JUNWOOJOK <78505823+JUNWOOJOK@users.noreply.github.com>
2022-04-29 01:49:38 +0900

    Update bowtie2_bash.sh

--- bowtie2_bash.sh
+++ bowtie2_bash.sh
@@ -1,9 +1,10 @@
-ls |grep -v bash|grep TCGA|sed 's/.d$//g' > all_file
+ls |grep -v bash|grep TCGA > all_file
 for i in {0..100..5}
 do
 for k in $(cat all_file|head -n $i|tail -n 5|tr '\n' ' ')
-do cd $i/trimmomatic
-bowtie2 -x -1 forward_paired.fq -2 reverse_paired.fq -U  singleton.fq  &
+do cd $k/trimmomatic
+bowtie2 --threads 2 -x /home/junwoojo/TCGA/done_file/human_bowtie2/human -1 forward_paired.fq -2 reverse_paired.fq -U  singleton.fq  -S $k.sam &
+cd ../../
 done
 wait
 done
