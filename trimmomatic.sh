ca25bf941a7987b02a94c14226f5da38493c0c42
JUNWOOJOK <78505823+JUNWOOJOK@users.noreply.github.com>
2022-04-28 15:33:12 +0900

    Update trimmomatic.sh

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -1,8 +1,8 @@
 cat trimmomatic_bash 
 for i in $(ls|grep TCGA|grep -v bash)
 do cd $i
-trimmomatic PE -threads 7  repair.1.fq repair.2.fq forward_paired.fq forward_unpaird.fq reverse_paired.fq reverse_unpaird.fq ILLUMINACLIP:/home/syc/hdd_ext2/junwoojo/donefile_bash/adapter:2:30:10:2:True LEADING:3 TRAILING:3 &
-trimmomatic SE -threads 7  repair.s.fq single_end.fq ILLUMINACLIP:/home/syc/hdd_ext2/junwoojo/donefile_bash/adapter:2:30:10:2:True LEADING:3 TRAILING:3 &
+trimmomatic PE -threads 7  repair.1.fq repair.2.fq forward_paired.fq forward_unpaird.fq reverse_paired.fq reverse_unpaird.fq ILLUMINACLIP::2:30:10:2:True LEADING:3 TRAILING:3 &
+trimmomatic SE -threads 7  repair.s.fq single_end.fq ILLUMINACLIP::2:30:10:2:True LEADING:3 TRAILING:3 &
 wait
 mkdir trimmomatic
 mv forward* trimmomatic/
