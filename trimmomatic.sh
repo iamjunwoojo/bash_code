42ab8e90c06761d423e82e4223137869747f3886
JUNWOOJOK <78505823+JUNWOOJOK@users.noreply.github.com>
2022-05-12 14:27:36 +0900

    Update trimmomatic.sh

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -1,4 +1,3 @@
-find
 for i in $(ls|grep TCGA|grep -v bash)
 do cd $i
 trimmomatic PE -threads 7  repair.1.fq repair.2.fq forward_paired.fq forward_unpaired.fq reverse_paired.fq reverse_unpaired.fq ILLUMINACLIP:/home/junwoojo/donefile_energy/adapter:2:30:10:2:True LEADING:3 TRAILING:3 &
