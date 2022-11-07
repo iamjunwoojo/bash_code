7545ea53be3f42131878c34ec0dcc1a19adfe4f4
junwoojo <78505823+JUNWOOJOK@users.noreply.github.com>
2022-11-07 11:37:15 +0900

    Update make_bonelgu_life_sample.sh

--- make_bonelgu_life_sample.sh
+++ make_bonelgu_life_sample.sh
@@ -2,6 +2,6 @@ mkdir bonelgu_${1}_${2}
 for i in $(ls|grep TCGA)
 do
 mkdir bonelgu_${1}_${2}/${i}
-cp -r ${i}/megahit_out   bonelgu_${1}_${2}/${i}/megahit_out
-rm -r  bonelgu_${1}_${2}/${i}/megahit_out/intermediate_contigs bonelgu_${1}_${2}/${i}/megahit_out/log bonelgu_${1}_${2}/${i}/megahit_out/options.json bonelgu_${1}_${2}/${i}/megahit_out/checkpoints.txt bonelgu_${1}_${2}/${i}/megahit_out/done
+mkdir bonelgu_${1}_${2}/${i}/megahit_out
+cp ${i}/megahit_out/final.contigs.fa   bonelgu_${1}_${2}/${i}/megahit_out/
 done
