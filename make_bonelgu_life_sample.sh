603c9ded1239172f0986ed38e57488e5f01f4a9d
junwoojo <78505823+JUNWOOJOK@users.noreply.github.com>
2022-11-07 11:39:51 +0900

    Update make_bonelgu_life_sample.sh

--- make_bonelgu_life_sample.sh
+++ make_bonelgu_life_sample.sh
@@ -4,4 +4,5 @@ do
 mkdir bonelgu_${1}_${2}/${i}
 mkdir bonelgu_${1}_${2}/${i}/megahit_out
 cp ${i}/megahit_out/final.contigs.fa   bonelgu_${1}_${2}/${i}/megahit_out/
+cp ${i}/flagstat* bonelgu_${1}_${2}/${i}/
 done
